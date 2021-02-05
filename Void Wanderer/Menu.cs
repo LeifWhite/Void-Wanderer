bc95c4ba8410a2c08bc9321de712a807ff9618ef
Leif White <leifw@ksu.edu>
2021-02-05 12:51:53 -0600

    Started menu class

---
+++ Void Wanderer/Menu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Void_Wanderer.Collisions;
+using Microsoft.Xna.Framework.Content;
+
+namespace Void_Wanderer
+{
+    public class Menu
+    {
+
+
+        private InputManager inputManager;
+
+
+        public void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            inputManager = new InputManager();
+
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+
+
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+            // TODO: Add your update logic here
+
+
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+
+
+
+        }
+    }
+}
